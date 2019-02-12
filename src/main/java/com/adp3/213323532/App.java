3121ee637d0e8363d8940cd4079f000629113ef5
JasonDaniels <jason.daniels@soundbyte.co.za>
2019-02-12 14:23:33 +0200

    Added Hello Jason! file

---
+++ src/main/java/com/adp3/213323532/App.java
@@ -0,0 +1,13 @@
+package com.adp3.213323532;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jason!" );
+    }
+}
---
+++ src/test/java/com/adp3/213323532/AppTest.java
@@ -0,0 +1,20 @@
+package com.adp3.213323532;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
